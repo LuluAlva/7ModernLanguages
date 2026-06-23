76e59731a5244cc1abc4b5e54d4b8782c4554f55
Lulu <139498094+LuluAlva@users.noreply.github.com>
2026-06-23 06:35:23 +0000

    Complete exercised2 and 3

--- week1/day1/exercise_02.py
+++ week1/day1/exercise_02.py
@@ -1,1 +1,12 @@
+monthly_income = 5000
+monthly_expenses = 3200
 
+money_left = monthly_income - monthly_expenses
+
+savings_goal = 1000
+money_after_savings = money_left - savings_goal
+print("Money After Savings:", money_after_savings)
+
+print("Monthly Income:", monthly_income)
+print("Monthly Expenses:", monthly_expenses)
+print("Money Left:", money_left)
--- week1/day1/exercise_03.py
+++ week1/day1/exercise_03.py
@@ -1,1 +1,14 @@
+shares_owned = 15
+share_price = 125
 
+portfolio_value = shares_owned * share_price
+
+cash_balance = 500
+
+total_account_value = portfolio_value + cash_balance
+
+
+print("Shares Owned:", shares_owned)
+print("Share Price:", share_price)
+print("Portfolio Value:", portfolio_value)
+print("Total Account Value:", total_account_value)
