13b037fa4119fee1466eb68ce2ff86ae92771ad8
Lulu <139498094+LuluAlva@users.noreply.github.com>
2026-06-23 06:53:37 +0000

    Complete Week 1 Day 1 exercises

--- week1/day1/exercise_04.py
+++ week1/day1/exercise_04.py
@@ -1,1 +1,13 @@
+emails_sent = 250 
+open_rate = 40
 
+emails_opened = emails_sent * open_rate / 100
+
+print("Emails Sent:", emails_sent)
+print("Open Rate:", open_rate)
+print("Emails Opened:", emails_opened)
+
+click_rate = 10
+emails_clicked = emails_opened * click_rate /100
+
+print("Emails Clicked:", emails_clicked)
--- week1/day1/exercise_05.py
+++ week1/day1/exercise_05.py
@@ -1,1 +1,7 @@
+battery_capacity = 100
+power_usage_per_hour = 5
+hours_used = 2
 
+hours_until_empty = (hours_used * power_usage_per_hour) - battery_capacity
+
+print("Hours Until Empty:", hours_until_empty)
